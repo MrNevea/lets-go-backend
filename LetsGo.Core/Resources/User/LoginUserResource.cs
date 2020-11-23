9db2f30400c765eb3ab202a17e6544337cabb410
unknown <aninik9@gmail.com>
2020-11-23 01:04:38 +0100

    Create auth controller & models

---
+++ LetsGo.Core/Resources/User/LoginUserResource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LetsGo.Core.Resources.User
+{
+    public class LoginUserResource
+    {
+    }
+}
---
+++ LetsGo/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using LetsGo.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsGo.Api.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("Login")]
+        public IActionResult Login([FromBody] User user)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("Register")]
+        public IActionResult Register([FromBody] User user)
+        {
+            return Ok();
+        }
+    }
+}
